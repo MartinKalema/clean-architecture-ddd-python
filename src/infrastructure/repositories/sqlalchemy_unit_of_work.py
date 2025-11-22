ed3e243a5466dbc79d648155124a05f58543bef2
MartinKalema <kalemaaaaaaaa@gmail.com>
2025-11-22 10:58:21 +0300

    refactor: rename unused arguments in UoW
    - renamed exc_val and exc_tb to _exc_val and _exc_tb in __aexit__ to indicate they are unused

--- src/infrastructure/repositories/sqlalchemy_unit_of_work.py
+++ src/infrastructure/repositories/sqlalchemy_unit_of_work.py
@@ -12,7 +12,7 @@ class SqlAlchemyUnitOfWork(UnitOfWork):
         self.books = SQLBookRepository(self._session)
         return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, _exc_val, _exc_tb):
         if exc_type:
             await self.rollback()
         await self._session.close()
